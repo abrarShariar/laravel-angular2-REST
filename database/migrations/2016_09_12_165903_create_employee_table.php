8815b7f191700edf833add879f136ae61d7a207f
abrarShariar <shariarabrar@gmail.com>
2016-09-13 00:07:29 +0600

    database migration done | using mysql

---
+++ database/migrations/2016_09_12_165903_create_employee_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->date('DOB');
+            $table->string('description',100);    //description max = 100 char
+            $table->timestamps();          //created_at and updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+}
