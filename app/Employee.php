6c473c9bf2528f53b0feae45e7fc8d393d542463
abrarShariar <shariarabrar@gmail.com>
2016-09-13 00:06:14 +0600

    initail commit

---
+++ app/Employee.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    /*
+    * The table assocaiated with the model
+    *
+    */
+    protected $table = 'employee';
+}
