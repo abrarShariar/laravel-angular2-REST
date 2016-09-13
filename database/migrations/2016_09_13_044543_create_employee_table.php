47dc5c1a7a85648b37569c01cfd60b5dd27efb26
abrarShariar <shariarabrar@gmail.com>
2016-09-13 11:05:23 +0600

    DB schema changed | new migration

---
+++ database/migrations/2016_09_13_044543_create_employee_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->increments('id');     //primary key + auto increment
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->date('DOB');
+            $table->string('description',100);
+            $table->timestamps();       //'created_at' 'updated_at' cols
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+}
