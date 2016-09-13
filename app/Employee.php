a7b4607ef6dd0723c9b19d39119b138287a1eacb
abrarShariar <shariarabrar@gmail.com>
2016-09-14 00:48:51 +0600

    fillable and guarded added

--- app/Employee.php
+++ app/Employee.php
@@ -1,16 +1,15 @@
 <?php
 
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
     /*
     * The table assocaiated with the model
-    *
     */
     protected $table = 'employee';
     //mass-assignment issue
     protected $fillable = ['firstname','lastname','DOB','description'];
+    protected $guarded = ['id'];
 }
